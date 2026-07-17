e5e34cf9f82da494321638c4f8147d520f7b1021
chandrakant-umrekar <umrekarchandrakant@gmail.com>
2026-07-17 17:47:21 +0530

    logout functionality and auth middleware added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -147,4 +147,25 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { refreshToken: undefined },
+    },
+    { new: true }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("accessToken", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+export { registerUser, loginUser, logoutUser };
