ac003ca60d91897e9a36bb25cef7a1f63b0e1d02
chandrakant-umrekar <umrekarchandrakant@gmail.com>
2026-08-04 16:39:20 +0530

    Add like routes

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 app.use("/api/v1/users", userRouter);
 
@@ -30,5 +31,7 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use("/api/v1/tweets", tweetRouter);
+
+app.use("/api/v1/likes", likeRouter);
 
 export { app };
